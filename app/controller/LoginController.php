624997a948cbf121236acdfc5aba883f69593ab1
RozLopez <66281927+RozLopez@users.noreply.github.com>
2020-08-14 12:20:36 -0500

    Update - RP

--- app/controller/LoginController.php
+++ app/controller/LoginController.php
@@ -1,5 +1,5 @@
 <?php 
-	include('../Model/LoginModel.php');
+	include('../model/LoginModel.php');
 	$LoginModel= new LoginModel;
 	$action = $_POST["action"];
 	$user=trim($_POST['usuario']);
@@ -8,21 +8,21 @@
 	switch($action){
 
 	    case "serchUser":
-	        $tables = "Usuarios";
-	        $fields = " User";
-	        $where  = " User='" . $user . "' AND PasswordEncrip='" . sha1($psw) . "' AND Activo='S' ";
+	        $tables = "usuario";
+	        $fields = " Nombre, IdUsuarios";
+	        $where  = " Usuario='" . $user . "' AND PasswordEncrip='" . sha1($psw) . "' AND Activo='S' ";
 			$var = json_decode($LoginModel->serchUser($tables, $fields, $where), true);		
 	        $var2=$var['data'];
 	        if($var['total'] != "0"){
 				$msg="Usuario encontrado";			
 				session_start();	
-				$_SESSION["user"] = $var2[0]['IdUsuario'];
+				$_SESSION["user"] = $var2[0]['IdUsuarios'];
 				$_SESSION["name"] = $var2[0]['Usuario'];
-				//$_SESSION["shortName"] = $var2[0]['NombreCorto'];		
-				print "<meta http-equiv='refresh' content='0;url=../../index.php'>";
+				print "<meta http-equiv='refresh' content='0;url=../../main.php'>";
 			}else{
+
 				$msg="Usuario y/o contraseña incorrectos favor de verificar";
-				print "<meta http-equiv='refresh' content='0;url=../../login.html?msg=" . $msg . "'>";		
+				print "<meta http-equiv='refresh' content='0;url=../../index.html?msg=" . $msg . "'>";		
 			}   
 	    break;
 
